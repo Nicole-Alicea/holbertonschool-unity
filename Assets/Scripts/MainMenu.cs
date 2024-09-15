9421fc61fe2709ef6e52dcfe1fd03243541c5000
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-14 21:10:49 -0400

    adding comments

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,29 +6,29 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public Material trapMat;
-    public Material goalMat;
-    public Toggle colorblindMode;
+    public Material trapMat; // Trap material
+    public Material goalMat; // Goal material
+    public Toggle colorblindMode; // Toggle to switch between normal and colorblind mode for the colors in the maze
     
     public void PlayMaze()
     {
-        if (colorblindMode.isOn)
+        if (colorblindMode.isOn) // Checks if colorblind mode is selected
         {
-            trapMat.color = new Color32(255, 112, 0, 1);
-            goalMat.color = Color.blue;
+            trapMat.color = new Color32(255, 112, 0, 1); // Changes red to orange
+            goalMat.color = Color.blue; // changes green to blue
         }
         else
         {
-            trapMat.color = Color.red;
-            goalMat.color = Color.green;
+            trapMat.color = Color.red; // changes orange to red
+            goalMat.color = Color.green; // changes blue to green
         }
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Menu scene is at build index 0 so this adds 1 to it to load the maze scene
     }
 
     public void QuitMaze()
     {
-        Application.Quit();
+        Application.Quit(); // Quits the game
         Debug.Log("Quit Game");
     }
 }
